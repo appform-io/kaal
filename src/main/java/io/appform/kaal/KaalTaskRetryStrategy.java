f684cdb3cd6a1c9d9f908521994d17fb5c849aa6
Rivanshu Goyal <rivanshu.goyal@phonepe.com>
2023-07-19 15:41:21 +0530

    added javadoc

--- src/main/java/io/appform/kaal/KaalTaskRetryStrategy.java
+++ src/main/java/io/appform/kaal/KaalTaskRetryStrategy.java
@@ -1,7 +1,16 @@
 package io.appform.kaal;
 
+/**
+ * A strategy used by the {@link KaalScheduler} to determine if a retry run for the task needs to be scheduled or not
+ * in case the current run ends up with an exception
+ */
 public interface KaalTaskRetryStrategy<T extends KaalTask<T, R>, R> {
 
+    /**
+     * Determines if a retry run for this task is needed or not
+     * @param taskData Data for the recently completed run
+     * @return True if a retry run is needed, false otherwise
+     */
     boolean shouldRetry(final KaalTaskData<T, R> taskData);
 
 }
